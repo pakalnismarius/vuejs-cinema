60f9ab5f38cb04406b669426c2a308f24239422d
marius <pakalnis.marius@gmail.com>
2020-02-09 08:34:51 +0200

    Adding overview template

---
+++ src/components/Overview.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="overview">
+    <div class="main">
+      <movie-list v-bind:time="time"
+                  v-bind:genre="genre"
+                  v-bind:movies="movies"
+                  v-bind:day="day">
+      </movie-list>
+      <movie-filter><movie-filter/>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import MovieList from './MovieList.vue';
+  import MovieFilter from './MovieFilter.vue';
+  export default {
+    props: ["time", "genre", "movies", "day"],
+    components: {
+      MovieList,
+      MovieFilter
+    }
+  }
+</script>
